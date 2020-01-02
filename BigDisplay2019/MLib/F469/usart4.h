84a2a0eb8d40121c8c05178757bb29767f6346bd
Charles Mickey Nowell <mickeynowell@tmwfl.com>
2020-01-02 17:17:12 -0500

    Added comments.

--- BigDisplay2019/MLib/F469/usart4.h
+++ BigDisplay2019/MLib/F469/usart4.h
@@ -1,4 +1,17 @@
- #ifndef __USART4_H
+/**
+ * No warrantees or liability is assumed with this software. 
+ * This software is the property of Charles M. Nowell, Jr. 
+ * It is placed in the public domain for personal use only. 
+ * All commercial rights are reserved. 
+ * You may use this software to make a display(s) for yourself 
+ * or for a model club. You may not use this software to make a 
+ * product for sale, whether for profit or not. 
+ * Charles M. Nowell, Jr 
+ * 2-Jan-2020 
+ * mickeysbigtimer@gmail.com 
+ *  
+ */
+#ifndef __USART4_H
 #define __USART4_H
 
 
--- BigDisplay2019/Src/FlashInterface.c
+++ BigDisplay2019/Src/FlashInterface.c
@@ -1,3 +1,16 @@
+/**
+ * No warrantees or liability is assumed with this software. 
+ * This software is the property of Charles M. Nowell, Jr. 
+ * It is placed in the public domain for personal use only. 
+ * All commercial rights are reserved. 
+ * You may use this software to make a display(s) for yourself 
+ * or for a model club. You may not use this software to make a 
+ * product for sale, whether for profit or not. 
+ * Charles M. Nowell, Jr 
+ * 2-Jan-2020 
+ * mickeysbigtimer@gmail.com 
+ *  
+ */
 #include "type.h"
 #include "stm32f4xx.h"
 #include "stm32f4xx_hal.h"
